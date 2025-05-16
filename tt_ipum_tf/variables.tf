57e59543985d737c52bce8b028211706a67169fc
Tomasz Trela WSL <pikson.tom@gmail.com>
2025-05-16 21:20:56 +0200

    1. Setup terraform

---
+++ tt_ipum_tf/variables.tf
@@ -0,0 +1,25 @@
+# variables.tf
+
+variable "github_token" {
+  description = "GitHub Personal Access Token"
+  type        = string
+  sensitive   = true # Marks this variable as sensitive, preventing it from appearing in logs and console output
+}
+
+variable "repository_name" {
+  description = "Name of the GitHub repository to create"
+  type        = string
+  default     = "ipum_terraform-managed-repo"
+}
+
+variable "repository_description" {
+  description = "Description of the GitHub repository"
+  type        = string
+  default     = "IPUM: Repository managed by Terraform"
+}
+
+variable "repository_publicly_visible" {
+  description = "Visibility of the GitHub repository"
+  type        = bool
+  default     = "false"
+}
